7e48cabdbab46c4f09f86b7ff1136b8f270aa925
SataNVme <kimyc8958@gmail.com>
2022-05-17 11:50:26 +0900

    main_build

--- src/main/java/com/project2/movieproject/controller/UserController.java
+++ src/main/java/com/project2/movieproject/controller/UserController.java
@@ -363,9 +363,14 @@ public class UserController {
     }
     
     @GetMapping("/userDetail")
-    public String userDetail(@RequestParam("user_id") String dbid) {
+    public String userDetail(@RequestParam("user_id") String dbid, Model model) {
     	
 		ArrayList<UserVO> userdata = userService.userdata(dbid);
+
+		ArrayList<UserVO> user_buy_list = userService.user_buy_list(userdata.get(0));
+		ArrayList<UserVO> user_rent_list = userService.user_rent_list(userdata.get(0));
+		model.addAttribute("user_buy_list", user_buy_list);
+		model.addAttribute("user_rent_list", user_rent_list);
     	
     	return "user/userDetail";
     }
